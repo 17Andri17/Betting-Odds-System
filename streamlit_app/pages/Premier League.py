744e7a9892072613cfac62d6dca1fce633fd02db
Owcakrzychu <krzysztofkosz02@gmail.com>
2024-12-08 23:48:45 +0100

    Usprawnienie wyglądu strony

--- streamlit_app/pages/Premier League.py
+++ streamlit_app/pages/Premier League.py
@@ -6,6 +6,7 @@ import pandas as pd
 def loadData():
     df=pd.read_csv("../premier_league_21_24_stadiums_weather.csv")
     df['date_x']=pd.to_datetime(df['date_x'])
+    df['date_x'] = df['date_x'].dt.strftime('%Y-%m-%d %H:%M')
     return df.sort_values(by='date_x')
 
 def restartStats():
@@ -14,6 +15,15 @@ def restartStats():
             st.session_state[f"show_row_{i}"] = False
     st.session_state['round_filter2'] = round_filter2
     st.session_state['round_filter1'] = round_filter1
+
+st.markdown("""
+    <style>
+    .center-text {
+        text-align: center; /* Wyśrodkowanie w poziomie */
+        margin: auto;
+    }
+    </style>
+    """, unsafe_allow_html=True)
 
 df=loadData()
 df_filtered=df
@@ -57,20 +67,29 @@ def showStats(i):
     st.session_state[f"show_row_{i}"] = not st.session_state.get(f"show_row_{i}", False)
 
 for i in range(min(number_of_matches, df_filtered['home_team'].count())):
-    col1,col2,col3,col4,col5,col6,col7 = st.columns(7)
-    with col3:
-        st.write(df_filtered.iloc[i]['home_team'])
-    with col6:
-        st.write(df_filtered.iloc[i]['away_team'])
-    with col2:
-        st.write(df_filtered.iloc[i]['round'])
+    col1, col2, col3, col4 = st.columns([2,5,1,1])
     with col1:
-        st.write(df_filtered.iloc[i]['date_x'])
+        st.markdown(f"""
+                <div style="text-align: center; font-size: 15px;
+                    background-color: #f8f9fb; 
+                    padding: 20px; 
+                    box-shadow: 4px 4px 8px rgba(0.2, 0.2, 0.2, 0.2);">{df_filtered.iloc[i]['date_x']}
+                </div>""", unsafe_allow_html=True)
+    with col2:
+        st.markdown(f"""
+                <div style="text-align: center; font-size: 15px;
+                    background-color: #f8f9fb; 
+                    padding: 20px; 
+                    box-shadow: 4px 4px 8px rgba(0.2, 0.2, 0.2, 0.2);">{df_filtered.iloc[i]['home_team']} {df_filtered.iloc[i]['home_goals_x']} - {df_filtered.iloc[i]['away_goals_x']} {df_filtered.iloc[i]['away_team']}
+                </div>""", unsafe_allow_html=True)
+    with col3:
+        st.markdown(f"""
+                <div style="text-align: center; font-size: 15px;
+                    background-color: #f8f9fb; 
+                    padding: 20px; 
+                    box-shadow: 4px 4px 8px rgba(0.2, 0.2, 0.2, 0.2);">Kolejka {df_filtered.iloc[i]['round']} 
+                </div>""", unsafe_allow_html=True)
     with col4:
-        st.write(df_filtered.iloc[i]['home_goals_x'])
-    with col5:
-        st.write(df_filtered.iloc[i]['away_goals_x'])
-    with col7:
         st.button(
             "Pokaż statystyki",
             key=f"button_{i}",
@@ -80,49 +99,51 @@ for i in range(min(number_of_matches, df_filtered['home_team'].count())):
 
     # Wyświetlanie dodatkowych informacji pod wierszem, jeśli jest włączone
     if st.session_state.get(f"show_row_{i}", False):
-        st.markdown(f"<p style='text-align: center; font-size: 20px;'>Statystyki meczu {df_filtered.iloc[i]['home_team']}-{df_filtered.iloc[i]['away_team']}:</p>", unsafe_allow_html=True)
-        st.markdown(f"<p style='text-align: center; font-size: 20px;'>Sędzia: {df_filtered.iloc[i]['referee']}</p>", unsafe_allow_html=True)
-        stats1, name, stats2 = st.columns(3)
-        with stats1:
-           st.write(df_filtered.iloc[i]["Ball Possession_home"])
-           st.write(df_filtered.iloc[i]["Goal Attempts_home"])
-           st.write(df_filtered.iloc[i]["Shots on Goal_home"])
-           st.write(df_filtered.iloc[i]["Free Kicks_home"])
-           st.write(df_filtered.iloc[i]["Corner Kicks_home"])
-           st.write(df_filtered.iloc[i]["Offsides_home"])
-           st.write(df_filtered.iloc[i]["Goalkeeper Saves_home"])
-           st.write(df_filtered.iloc[i]["Fouls_home"])
-           st.write(df_filtered.iloc[i]["Yellow Cards_home"])
-           st.write(df_filtered.iloc[i]["Red Cards_home"])
-           st.write(df_filtered.iloc[i]["Total Passes_home"])
-           st.write(df_filtered.iloc[i]["Completed Passes_home"])
+        row = df_filtered.iloc[i]
+        st.markdown(f"""
+                <div style="text-align: center; font-size: 15px;
+                    background-color: #f8f9fa; 
+                    border-radius: 10px; 
+                    padding: 20px; 
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                    <p style='text-align: center; font-size: 20px;'>Statystyki meczu {df_filtered.iloc[i]['home_team']} - {df_filtered.iloc[i]['away_team']}:</p>
+                    <p style='text-align: center; font-size: 20px;'>Sędzia: {df_filtered.iloc[i]['referee']}</p>
+                </div>""", unsafe_allow_html=True)
+        stats, graphs = st.columns(2)
+        with stats:
+            categories = ["Posiadanie piłki", "Strzały", "Strzały na bramkę", "Rzuty wolne", "Rzuty rózne",
+                "Spalone", "Obrony bramkarskie", "Faule", "Żółte kartki", "Czerwone kartki", "Podania", "Celne podania"]
+            values_home = [df_filtered.iloc[i]["Ball Possession_home"], df_filtered.iloc[i]["Goal Attempts_home"],
+                df_filtered.iloc[i]["Shots on Goal_home"], df_filtered.iloc[i]["Free Kicks_home"],
+                df_filtered.iloc[i]["Corner Kicks_home"], df_filtered.iloc[i]["Offsides_home"],
+                df_filtered.iloc[i]["Goalkeeper Saves_home"], df_filtered.iloc[i]["Fouls_home"],
+                df_filtered.iloc[i]["Yellow Cards_home"], df_filtered.iloc[i]["Red Cards_home"],
+                df_filtered.iloc[i]["Total Passes_home"], df_filtered.iloc[i]["Completed Passes_home"]]
+            values_away = [df_filtered.iloc[i]["Ball Possession_away"], df_filtered.iloc[i]["Goal Attempts_away"],
+                df_filtered.iloc[i]["Shots on Goal_away"], df_filtered.iloc[i]["Free Kicks_away"],
+                df_filtered.iloc[i]["Corner Kicks_away"], df_filtered.iloc[i]["Offsides_away"],
+                df_filtered.iloc[i]["Goalkeeper Saves_away"], df_filtered.iloc[i]["Fouls_away"],
+                df_filtered.iloc[i]["Yellow Cards_away"], df_filtered.iloc[i]["Red Cards_away"],
+                df_filtered.iloc[i]["Total Passes_away"], df_filtered.iloc[i]["Completed Passes_away"]]
 
-        with name:
-           st.write("Posiadanie piłki")
-           st.write("Strzały")
-           st.write("Strzały na bramkę")
-           st.write("Rzuty wolne")
-           st.write("Rzuty rożne")
-           st.write("Spalone")
-           st.write("Obrony bramkarzy")
-           st.write("Faule")
-           st.write("Żółte kartki")
-           st.write("Czerwone kartki")
-           st.write("Podania")
-           st.write("Celne podania")
-
-        with stats2:
-           st.write(df_filtered.iloc[i]["Ball Possession_away"])
-           st.write(df_filtered.iloc[i]["Goal Attempts_away"])
-           st.write(df_filtered.iloc[i]["Shots on Goal_away"])
-           st.write(df_filtered.iloc[i]["Free Kicks_away"])
-           st.write(df_filtered.iloc[i]["Corner Kicks_away"])
-           st.write(df_filtered.iloc[i]["Offsides_away"])
-           st.write(df_filtered.iloc[i]["Goalkeeper Saves_away"])
-           st.write(df_filtered.iloc[i]["Fouls_away"])
-           st.write(df_filtered.iloc[i]["Yellow Cards_away"])
-           st.write(df_filtered.iloc[i]["Red Cards_away"])
-           st.write(df_filtered.iloc[i]["Total Passes_away"])
-           st.write(df_filtered.iloc[i]["Completed Passes_away"])
+            data = """
+                <div style="text-align: center; font-size: 15px;
+                    background-color: #f8f9fa; 
+                    border-radius: 10px; 
+                    padding: 20px; 
+                    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                """
+            for i in range(len(categories)):
+                data += f"""
+                <div style="display: flex; justify-content: center; margin-bottom: 15px;">
+                    <div style="width: 100px;">{values_home[i]}</div>
+                    <div style="width: 200px;">{categories[i]}</div>
+                    <div style="width: 100px;">{values_away[i]}</div>
+                </div>
+                """
+            data += "</div>"
+            st.markdown(data, unsafe_allow_html=True)
+        with graphs:
+            st.write("Miejsce na jakieś wykresy czy coś jeszcze nie wiem")
 
 st.session_state
